09733fbeda30a58f2f8cdec32cab448d5dd09b07
Alois Kouba <koubaalo@fit.cvut.cz>
2021-11-21 13:20:05 +0100

    Added remaining template views

--- backend/reservations/urls.py
+++ backend/reservations/urls.py
@@ -16,9 +16,40 @@ router.register(r"^rooms", reservations.views.RoomViewSet)
 
 urlpatterns = [
     path(r"api/", include(router.urls)),
+
     path('persons/<int:person_id>/', reservations.views.PersonTemplateView.person_get_view, name='person_view'),
     path('persons/', reservations.views.PersonTemplateView.persons_get_view, name='person_list'),
     path('persons/<int:person_id>/delete', reservations.views.PersonTemplateView.person_delete_view, name='person_delete'),
     path('persons/create', reservations.views.PersonTemplateView.person_create_view, name='person_create'),
     path('persons/<int:person_id>/edit', reservations.views.PersonTemplateView.person_edit_view, name='person_update'),
+
+    path('buildings/<int:building_id>/', reservations.views.BuildingTemplateView.building_get_view, name='building_view'),
+    path('buildings/', reservations.views.BuildingTemplateView.buildings_get_view, name='building_list'),
+    path('buildings/<int:building_id>/delete', reservations.views.BuildingTemplateView.building_delete_view, name='building_delete'),
+    path('buildings/create', reservations.views.BuildingTemplateView.building_create_view, name='person_create'),
+    path('buildings/<int:building_id>/edit', reservations.views.BuildingTemplateView.building_edit_view, name='building_update'),
+
+    path('rooms/<int:room_id>/', reservations.views.RoomTemplateView.room_get_view, name='room_view'),
+    path('rooms/', reservations.views.RoomTemplateView.rooms_get_view, name='room_list'),
+    path('rooms/<int:room_id>/delete', reservations.views.RoomTemplateView.room_delete_view, name='room_delete'),
+    path('rooms/create', reservations.views.RoomTemplateView.room_create_view, name='person_create'),
+    path('rooms/<int:room_id>/edit', reservations.views.RoomTemplateView.room_edit_view, name='room_update'),
+
+    path('groups/<int:group_id>/', reservations.views.GroupTemplateView.group_get_view, name='group_view'),
+    path('groups/', reservations.views.GroupTemplateView.groups_get_view, name='group_list'),
+    path('groups/<int:group_id>/delete', reservations.views.GroupTemplateView.group_delete_view, name='group_delete'),
+    path('groups/create', reservations.views.GroupTemplateView.group_create_view, name='person_create'),
+    path('groups/<int:group_id>/edit', reservations.views.GroupTemplateView.group_edit_view, name='group_update'),
+
+    path('reservationstatuses/<int:reservation_status_id>/', reservations.views.ReservationStatusTemplateView.reservation_status_get_view, name='reservation_status_view'),
+    path('reservationstatuses/', reservations.views.ReservationStatusTemplateView.reservation_statuses_get_view, name='reservation_status_list'),
+    path('reservationstatuses/<int:reservation_status_id>/delete', reservations.views.ReservationStatusTemplateView.reservation_status_delete_view, name='reservation_status_delete'),
+    path('reservationstatuses/create', reservations.views.ReservationStatusTemplateView.reservation_status_create_view, name='reservation_status_create'),
+    path('reservationstatuses/<int:reservation_status_id>/edit', reservations.views.ReservationStatusTemplateView.reservation_status_edit_view, name='reservation_status_update'),
+
+    path('reservations/<int:reservation_id>/', reservations.views.ReservationTemplateView.reservation_get_view, name='reservation_view'),
+    path('reservations/', reservations.views.ReservationTemplateView.reservations_get_view, name='reservation_list'),
+    path('reservations/<int:reservation_id>/delete', reservations.views.ReservationTemplateView.reservation_delete_view, name='reservation_delete'),
+    path('reservations/create', reservations.views.ReservationTemplateView.reservation_create_view, name='person_create'),
+    path('reservations/<int:reservation_id>/edit', reservations.views.ReservationTemplateView.reservation_edit_view, name='reservation_update'),
 ]
--- backend/reservations/views.py
+++ backend/reservations/views.py
@@ -1,16 +1,12 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect
 from django.template import loader
-from django.views import View
-
 from rest_framework import viewsets
-
 from reservations.serializers import *
 from reservations.models import *
-
 from reservations.services import *
-
 from reservations.forms import *
+from datetime import datetime
 
 
 class PersonViewSet(viewsets.ModelViewSet):
@@ -81,5 +77,231 @@ class PersonTemplateView:
         if form.is_valid():
             form.save()
             return redirect("/persons/")
+        template = loader.get_template('reservations/test_create.html')
+        return HttpResponse(template.render({'form': form}, request))
+
+
+class BuildingTemplateView:
+    @staticmethod
+    def building_get_view(request, building_id):
+        building = BuildingService.find_by_id(building_id)
+        template = loader.get_template('reservations/test_list.html')
+        return HttpResponse(template.render({'test_list': building}, request))
+
+    @staticmethod
+    def buildings_get_view(request):
+        buildings = BuildingService.find_all()
+        template = loader.get_template('reservations/test_list.html')
+        return HttpResponse(template.render({'test_list': buildings}, request))
+
+    @staticmethod
+    def building_delete_view(request, building_id):
+        template = loader.get_template('reservations/test_list.html')
+        if not BuildingService.delete(building_id):
+            return HttpResponse(template.render({"errors": ["Failed to delete building"]}, request))
+        return redirect("/buildings/")
+
+    @staticmethod
+    def building_create_view(request):
+        form = BuildingForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect("/buildings/")
+        template = loader.get_template('reservations/test_create.html')
+        return HttpResponse(template.render({'form': form}, request))
+
+    @staticmethod
+    def building_edit_view(request, building_id):
+        instance = BuildingService.find_by_id(building_id)
+        if instance is None:
+            raise Http404("Building does not exist")
+        form = BuildingForm(request.POST or None, instance=instance)
+        if form.is_valid():
+            form.save()
+            return redirect("/buildings/")
+        template = loader.get_template('reservations/test_create.html')
+        return HttpResponse(template.render({'form': form}, request))
+
+
+class GroupTemplateView:
+    @staticmethod
+    def group_get_view(request, group_id):
+        group = GroupService.find_by_id(group_id)
+        template = loader.get_template('reservations/test_list.html')
+        return HttpResponse(template.render({'test_list': group}, request))
+
+    @staticmethod
+    def groups_get_view(request):
+        groups = GroupService.find_all()
+        template = loader.get_template('reservations/test_list.html')
+        return HttpResponse(template.render({'test_list': groups}, request))
+
+    @staticmethod
+    def group_delete_view(request, group_id):
+        template = loader.get_template('reservations/test_list.html')
+        if not GroupService.delete(group_id):
+            return HttpResponse(template.render({"errors": ["Failed to delete group"]}, request))
+        return redirect("/groups/")
+
+    @staticmethod
+    def group_create_view(request):
+        form = GroupForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect("/groups/")
+        template = loader.get_template('reservations/test_create.html')
+        return HttpResponse(template.render({'form': form}, request))
+
+    @staticmethod
+    def group_edit_view(request, group_id):
+        instance = GroupService.find_by_id(group_id)
+        if instance is None:
+            raise Http404("Building does not exist")
+        form = GroupForm(request.POST or None, instance=instance)
+        if form.is_valid():
+            form.save()
+            return redirect("/groups/")
+        template = loader.get_template('reservations/test_create.html')
+        return HttpResponse(template.render({'form': form}, request))
+
+
+class RoomTemplateView:
+    @staticmethod
+    def room_get_view(request, room_id):
+        room = RoomService.find_by_id(room_id)
+        template = loader.get_template('reservations/test_list.html')
+        return HttpResponse(template.render({'test_list': room}, request))
+
+    @staticmethod
+    def rooms_get_view(request):
+        rooms = RoomService.find_all()
+        template = loader.get_template('reservations/test_list.html')
+        return HttpResponse(template.render({'test_list': rooms}, request))
+
+    @staticmethod
+    def room_delete_view(request, room_id):
+        template = loader.get_template('reservations/test_list.html')
+        if not RoomService.delete(room_id):
+            return HttpResponse(template.render({"errors": ["Failed to delete room"]}, request))
+        return redirect("/rooms/")
+
+    @staticmethod
+    def room_create_view(request):
+        form = RoomForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect("/rooms/")
+        template = loader.get_template('reservations/test_create.html')
+        return HttpResponse(template.render({'form': form}, request))
+
+    @staticmethod
+    def room_edit_view(request, room_id):
+        instance = RoomService.find_by_id(room_id)
+        if instance is None:
+            raise Http404("Room does not exist")
+        form = RoomForm(request.POST or None, instance=instance)
+        if form.is_valid():
+            form.save()
+            return redirect("/rooms/")
+        template = loader.get_template('reservations/test_create.html')
+        return HttpResponse(template.render({'form': form}, request))
+
+
+class ReservationStatusTemplateView:
+    @staticmethod
+    def reservation_status_get_view(request, reservation_status_id):
+        reservation_status = ReservationStatusService.find_by_id(reservation_status_id)
+        template = loader.get_template('reservations/test_list.html')
+        return HttpResponse(template.render({'test_list': reservation_status}, request))
+
+    @staticmethod
+    def reservation_statuses_get_view(request):
+        reservation_statuses = ReservationStatusService.find_all()
+        template = loader.get_template('reservations/test_list.html')
+        return HttpResponse(template.render({'test_list': reservation_statuses}, request))
+
+    @staticmethod
+    def reservation_status_delete_view(request, reservation_status_id):
+        template = loader.get_template('reservations/test_list.html')
+        if not ReservationStatusService.delete(reservation_status_id):
+            return HttpResponse(template.render({"errors": ["Failed to delete reservation status"]}, request))
+        return redirect("/reservationstatuses/")
+
+    @staticmethod
+    def reservation_status_create_view(request):
+        form = ReservationStatusForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect("/reservationstatuses/")
+        template = loader.get_template('reservations/test_create.html')
+        return HttpResponse(template.render({'form': form}, request))
+
+    @staticmethod
+    def reservation_status_edit_view(request, reservation_status_id):
+        instance = ReservationStatusService.find_by_id(reservation_status_id)
+        if instance is None:
+            raise Http404("Reservation status does not exist")
+        form = ReservationStatusForm(request.POST or None, instance=instance)
+        if form.is_valid():
+            form.save()
+            return redirect("/reservationstatuses/")
+        template = loader.get_template('reservations/test_create.html')
+        return HttpResponse(template.render({'form': form}, request))
+
+
+class ReservationTemplateView:
+    @staticmethod
+    def reservation_get_view(request, reservation_id):
+        reservation = ReservationService.find_by_id(reservation_id)
+        template = loader.get_template('reservations/test_list.html')
+        return HttpResponse(template.render({'test_list': reservation}, request))
+
+    @staticmethod
+    def reservations_get_view(request):
+        reservations = ReservationService.find_all()
+        template = loader.get_template('reservations/test_list.html')
+        return HttpResponse(template.render({'test_list': reservations}, request))
+
+    @staticmethod
+    def reservation_delete_view(request, reservation_id):
+        template = loader.get_template('reservations/test_list.html')
+        if not ReservationService.delete(reservation_id):
+            return HttpResponse(template.render({"errors": ["Failed to delete reservation"]}, request))
+        return redirect("/reservations/")
+
+    @staticmethod
+    def reservation_create_view(request):
+        form = ReservationForm(request.POST or None)
+        if form.is_valid():
+            timestamp = datetime.now()
+            reservation_status = ReservationStatus()
+            reservation_status.author = form.cleaned_data.get('author')
+            reservation_status.modified = timestamp
+            reservation_status.note = form.cleaned_data.get('note')
+            reservation_status.save()
+
+            reservation = Reservation()
+            reservation.author = form.cleaned_data.get('author')
+            reservation.dt_from = form.cleaned_data.get('dt_from')
+            reservation.dt_to = form.cleaned_data.get('dt_to')
+            reservation.dt_to = form.cleaned_data.get('dt_to')
+            reservation.dt_created = timestamp
+            reservation.room = form.cleaned_data.get('room')
+            reservation.save()
+            reservation.attendees.set(form.cleaned_data.get('attendees'))
+            reservation.reservation_status.set([reservation_status])
+            return redirect("/reservations/")
+        template = loader.get_template('reservations/test_create.html')
+        return HttpResponse(template.render({'form': form}, request))
+
+    @staticmethod
+    def reservation_edit_view(request, reservation_id):
+        instance = ReservationService.find_by_id(reservation_id)
+        if instance is None:
+            raise Http404("Reservation does not exist")
+        form = ReservationForm(request.POST or None, instance=instance)
+        if form.is_valid():
+            form.save()
+            return redirect("/reservations/")
         template = loader.get_template('reservations/test_create.html')
         return HttpResponse(template.render({'form': form}, request))
