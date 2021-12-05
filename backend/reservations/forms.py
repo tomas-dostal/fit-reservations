078d66b5aef8ccaad360a84bc1b0ce87ed576a26
Tomas Dostal <dostato6@fit.cvut.cz>
2021-12-05 18:27:29 +0100

    Linter

--- backend/reservations/forms.py
+++ backend/reservations/forms.py
@@ -1,5 +1,5 @@
 import django.forms as forms
-from django.forms import ModelForm, SelectDateWidget, DateInput, DateTimeInput
+from django.forms import ModelForm, DateTimeInput
 from reservations.models import *
 from reservations.services import *
 
@@ -66,8 +66,13 @@ class AdminReservationForm(ModelForm):
         model = Reservation
         # TODO: Add owner field, author=currentUser
         fields = ["author", "attendees", "room", "dt_from", "dt_to"]
-        labels = {"author": "Autor", "attendees": "Uživatelé", "room": "Místnost",
-                  "fd_from": "Platnost od", "fd_to": "Platnost do"}
+        labels = {
+            "author": "Autor",
+            "attendees": "Uživatelé",
+            "room": "Místnost",
+            "fd_from": "Platnost od",
+            "fd_to": "Platnost do",
+        }
         widgets = {
             "dt_from": DateTimeInput(attrs={"type": "datetime-local"}),
             "dt_to": DateTimeInput(attrs={"type": "datetime-local"}),
@@ -88,8 +93,13 @@ class ReservationForm(ModelForm):
         model = Reservation
         # TODO: author=currentUser, owner = currectUser, room - display only rooms available for currentUser
         fields = ["author", "attendees", "room", "dt_from", "dt_to"]
-        labels = {"author": "Autor", "attendees": "Uživatelé", "room": "Místnost",
-                  "fd_from": "Platnost od", "fd_to": "Platnost do"}
+        labels = {
+            "author": "Autor",
+            "attendees": "Uživatelé",
+            "room": "Místnost",
+            "fd_from": "Platnost od",
+            "fd_to": "Platnost do",
+        }
         widgets = {
             "dt_from": DateTimeInput(attrs={"type": "datetime-local"}),
             "dt_to": DateTimeInput(attrs={"type": "datetime-local"}),
