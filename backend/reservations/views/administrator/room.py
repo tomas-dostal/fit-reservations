5de2fcb1e6b5f2aaa1777bb78c696f1ce37c298d
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2021-12-22 22:34:12 +0100

    Improve template permissions.

--- backend/reservations/views/administrator/room.py
+++ backend/reservations/views/administrator/room.py
@@ -19,7 +19,8 @@ class AdminRoomViewSet(viewsets.ModelViewSet):
 
 class AdminRoomTemplateView(ListView):
     @staticmethod
-    @user_passes_test(lambda u: u.is_superuser)
+    @user_passes_test(lambda u: u.is_superuser or u.has_perm("reservations.is_group_manager") or u.has_perm(
+        "reservations.is_room_manager"))
     def room_get_view(request, room_id):
         room = RoomService.find_by_id(room_id)
         template = loader.get_template("administrator/rooms/detail.html")
@@ -42,8 +43,8 @@ class AdminRoomTemplateView(ListView):
             rooms = paginator.page(paginator.num_pages)
 
         template = "administrator/rooms/manager_list.html"
-        #if request.user.is_superuser:
-        #    template = "administrator/rooms/list.html"
+        if request.user.is_superuser:
+            template = "administrator/rooms/list.html"
 
         return render(request, template, {"rooms": rooms})
 
