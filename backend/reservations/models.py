42aed3ad33121f4393a19a06708cfe2c59e84351
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2021-12-22 16:56:11 +0100

    Reservation list and detail.

--- backend/reservations/models.py
+++ backend/reservations/models.py
@@ -140,4 +140,7 @@ class Reservation(models.Model):
             self.dt_to,
             self.author,
             str([str(a) for a in self.attendees.all()]),
-        )
+    )
+
+    def current_status(self):
+        return self.reservation_status.last().status if hasattr(self.reservation_status.last(), "status") else "None"
