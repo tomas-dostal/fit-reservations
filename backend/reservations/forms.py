76fd530a2ade45d52ca8bd72a896457be795d442
Alois Kouba <koubaalo@fit.cvut.cz>
2021-12-30 12:02:49 +0100

    Added room selections to Group form

--- backend/reservations/forms.py
+++ backend/reservations/forms.py
@@ -56,6 +56,14 @@ class BuildingForm(ModelForm):
 
 
 class GroupForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        instance = kwargs.get("instance", None)
+        if instance:
+            self.fields["rooms"].initial = RoomService.find_rooms_for_group(instance)
+
+    rooms = forms.ModelMultipleChoiceField(Room.objects.all(), label="Místnosti", required=False)
+
     class Meta:
         model = Group
         labels = {"name": "Název", "parent": "Nadřazená skupina", "member": "Členové", "manager": "Správce"}
